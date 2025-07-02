8390a5db145633556b1e747c14fe26d6215879be
sahmad1Conestoga <sahmad1@conestogac.on.ca>
2025-07-02 22:43:20 +0000

    added datatypes and conversion

---
+++ JS_Basics/conversionOperation.js
@@ -0,0 +1,52 @@
+let score = "56"
+//console.log(typeof(score));
+
+
+let convertedScore = Number(score);
+//console.log(typeof(convertedScore));
+
+
+let isLoggedIn = false
+let randomString = "33abc"
+//console.log(Number(randomString));
+ 
+/*
+"33" ==> 33
+"" or null  ==> 0
+"33abc" or undefined ==> undefined
+
+*/
+
+randomNumber = null
+let stringNumber = String(randomNumber)
+// console.log(stringNumber) // null
+// console.log(typeof stringNumber); //string
+
+
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2*3);
+//console.log(2%2);//0
+//console.log(2**3); //8
+
+
+let firstName = 777
+let middleName = "Saim"
+let lastName = "Ahmad"
+
+//console.log(firstName+middleName+lastName);
+
+// console.log(1 + 2+ "2") //32
+// console.log("1" + 2+ 2) //122
+
+// console.log(+true) // 1
+// console.log(+"") // 0
+
+
+let gameCOunter = 100
+gameCOunter++
+console.log((gameCOunter));
---
+++ JS_Basics/intro.js
@@ -0,0 +1,41 @@
+//console.log ("Hello World")
+
+const accountID = 2345
+let accountEmail = "saim@google.com"
+var accountPassword = "password"
+accountCity = "Waterloo"
+let accountProvince;
+
+//console.log(accountProvince);
+
+//console.log(typeof(accountEmail))
+
+//console.log("User account ID is: "+accountID+ "and account Email is: "+accountEmail);
+//console.log(`User account ID is ${accountID} and account Email is ${accountEmail}`)
+
+
+console.log(3
+    +
+    3)
+
+let name = "Ahmad"
+let age =18
+let isLogged = false
+let province
+
+console.log(typeof(province));
+
+
+
+/*
+string
+number
+boolen
+undefined
+null
+*/
+
+console.log(typeof(undefined))
+console.log(typeof(null))
+
+
